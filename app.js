1958d572d5c3fd3e23199543ecc2eae036e05a30
Abhinav T K <63438668+abhinavtk@users.noreply.github.com>
2021-08-20 23:54:02 +0530

    Update: app.js and styles.css

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+var serverUrl = "https://api.funtranslations.com/translate/pirate.json"
 
 function getTranslationUrl(text) {
     return serverUrl + "?text=" + text;
